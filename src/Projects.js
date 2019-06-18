cf8dfece7240f872b915262915647d262a75ea7b
Jonathan Guiang <jkguiang@gmail.com>
2019-06-17 21:39:08 -0700

    added real projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,20 +1,23 @@
 import React, { Component, Fragment } from 'react';
-import { Container, Row, Card, CardImg, CardText, CardBody,
+import { Container, Row, Col, Card, CardImg, CardText, CardBody,
          CardTitle, CardFooter, Button } from 'reactstrap';
 import { SwingLeftContent } from './Animations.js';
+import { projects } from './text.js';
 
 class ProjectCard extends Component {
     // Card, Short description, thumbnail
     render() {
-        var thisURL = "/projects/"+this.props.name;
         return (
-          <Card>
-            <CardImg top src="https://via.placeholder.com/1000x500" alt="Card image cap" />
+          <Card className="h-100">
+            <CardImg top src={this.props.image} alt="Card image cap" />
             <CardBody className="text-left">
-              <CardTitle>Title</CardTitle>
-              <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu eros nisl. Donec sollicitudin nisl nisi, sit amet rutrum nibh faucibus sit amet.</CardText>
+              <CardTitle>
+                <h4 className="mb-0">{this.props.name}</h4>
+                <p><i>{this.props.type}</i></p>
+              </CardTitle>
+              <CardText>{this.props.desc}</CardText>
             </CardBody>
-            <CardFooter><Button href="https://via.placeholder.com/400x300" outline>Learn More &raquo;</Button></CardFooter>
+            <CardFooter><Button href={this.props.href} outline>Learn More &raquo;</Button></CardFooter>
           </Card>
         );
     }
@@ -23,6 +26,14 @@ class ProjectCard extends Component {
 class Projects extends Component {
   render() {
     const pose = this.props.isVisible ? "visible" : "hidden";
+    var counter = 0;
+    const cards = (projects).map((p) =>
+        <Col md="4" className="d-flex align-items-stretch pb-4" key={counter++}>
+          <SwingLeftContent pose={pose}>
+            <ProjectCard image={p.image} name={p.name} type={p.type} desc={p.desc} href={p.href}/>
+          </SwingLeftContent>
+        </Col>
+    );
     return (
         <Fragment>
           <SwingLeftContent pose={pose}><h1 className="display-4">Projects</h1></SwingLeftContent>
@@ -31,15 +42,7 @@ class Projects extends Component {
           </p></SwingLeftContent>
           <Container className="vh-25">
             <Row className="mx-0">
-              <div className="col-md-4 d-flex align-items-stretch"><SwingLeftContent pose={pose}>
-                <ProjectCard/>
-              </SwingLeftContent></div>
-              <div className="col-md-4 d-flex align-items-stretch"><SwingLeftContent pose={pose}>
-                <ProjectCard/>
-              </SwingLeftContent></div>
-              <div className="col-md-4 d-flex align-items-stretch"><SwingLeftContent pose={pose}>
-                <ProjectCard/>
-              </SwingLeftContent></div>
+              { cards }
             </Row>
           </Container>
         </Fragment>
