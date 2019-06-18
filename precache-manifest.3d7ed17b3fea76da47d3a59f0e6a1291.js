5ebc6c955f148ea506e4ab2d75d3e5b12837aaeb
Jonathan Guiang <jkguiang@gmail.com>
2019-06-17 21:43:53 -0700

    Updates

---
+++ precache-manifest.3d7ed17b3fea76da47d3a59f0e6a1291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92ed1ce270449ac7c3f9894564c34a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9dd4d758b4eb3bd07af",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a7ff0819951031cf1209",
+    "url": "/static/css/main.0a3122df.chunk.css"
+  },
+  {
+    "revision": "f9dd4d758b4eb3bd07af",
+    "url": "/static/js/2.180820a2.chunk.js"
+  },
+  {
+    "revision": "a7ff0819951031cf1209",
+    "url": "/static/js/main.958efb46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d20a3e4431287323a8cb1f30b92208a",
+    "url": "/static/media/latex_cv.4d20a3e4.pdf"
+  }
+]);
