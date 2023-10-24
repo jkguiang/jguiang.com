fbcbd87f4f7a4a304b28e3c85b52015e5b4d476e
Jonathan Guiang <jkguiang@gmail.com>
2023-10-24 10:51:48 -0700

    updated website text

--- src/assets/text.js
+++ src/assets/text.js
@@ -1,16 +1,49 @@
 // Images for "Experiences" section
 import gsoc from './gsoc.png';
-import ucsb_physics from './ucsb_physics.jpg';
+import ucsd_seal from './ucsd_seal.png';
+import ucsb_seal from './ucsb_seal.png';
 import pramantha from './pramantha.png';
 // Images for "Education" section
 import ucsd from './ucsd.png';
 import ucsb from './ucsb.png';
 // Images for "Projects" section
-import integrals from './integrals.jpg';
+import ct_scans from './ct_scans.jpg';
+import integratable from './integratable.png';
+import rapido from './rapido.png';
+import nbc from './nbc_investigates.png';
 import sbhacks_v from './sbhacks_v.jpg';
 import cover from './cover.jpg';
 
 const experiences = [
+    {
+        "employer": "Würthwein-Yagil Group",
+        "position": "Graduate Student Researcher",
+        "duration": "Jan. 2020 - Present",
+        "location": "San Diego, CA",
+        "image": ucsd_seal,
+        "highlights": [
+            {
+                "name": "VBS Higgs Analyses",
+                "desc": "Performed a variety of searches for anomalous Higgs boson couplings via the production of a Higgs and vector boson(s) by vector boson scattering. Wrote performant C++ analysis code to down-select petabytes of CMS proton-proton collision data to the megabytes relevant to analysis. Leveraged Python-based data science tools to turn that data into physics insights. Synthesized results into technical presentations and scientific publications (inpreparation) presented to an international audience of peers."
+            },
+            {
+                "name": "Particle Tracking ML",
+                "desc": "Explored and implemented machine learning (DNNs, GNNs) solutions for resolving particle tracks out of massive point clouds with large-throughput and high-efficiency. Designed ML training pipeline, compared performance against existing track quality selections, and successfully incorporated ML into a highly parallelizable prototype."
+            },
+            {
+                "name": "Rucio-SENSE Interoperation",
+                "desc": "Helped manage 3 petabytes of CMS data stored at the UCSD Tier 2 computing facility which services thousands of scientists. Assisted in transfer of this data to a new system."
+            },
+            {
+                "name": "XRootD HTTPS Benchmarking",
+                "desc": "Helped benchmark XRootD file-transfer performance when using HTTPS in order to show that it can provide the 500 Gb/s transfer speeds needed for the HL-LHC."
+            },
+            {
+                "name": "US-CMS Tier 2 Data Manager",
+                "desc": "Conceptualized, designed, and implemented a statistical tool for data quality management with an online graphical interface for ease of use. Continued collaboration with another student in Switzerland to further improve the platform and market it to other research groups."
+            },
+        ]
+    },
     {
         "employer": "Google Summer of Code",
         "position": "Student Developer",
@@ -29,16 +62,12 @@ const experiences = [
         ]
     },
     {
-        "employer": "UC Santa Barbara",
+        "employer": "Campagnari Group",
         "position": "Undergraduate Student Researcher",
         "duration": "Dec. 2016 - Jun. 2019",
         "location": "Santa Barbara, CA",
-        "image": ucsb_physics,
+        "image": ucsb_seal,
         "highlights": [
-            {
-                "name": "Overview",
-                "desc": "Worked on a number of projects for a CMS High Energy research group led by Dr. Claudio Campagnari. Developed a number of experimentalist skills from circuit design and hardware construction to software development and data analysis."
-            },
             {
                 "name": "Search for Rare Higgs Decays",
                 "desc": "Novel search for H to rho/phi+gamma decays using events from a sample of proton-proton collisions collected with the CMS detector to probe Higgs couplings to light, flavored quarks."
@@ -71,14 +100,14 @@ const experiences = [
 const universities = [
     {
         "university": "UC San Diego",
-        "degree": "PhD. Physics",
+        "degree": "M.S. Physics, PhD. Physics",
         "duration": "Sep. 2019 - Present",
         "location": "San Diego, CA",
         "image": ucsd,
         "highlights": [
             {
                 "name": "GPA",
-                "desc": "3.76"
+                "desc": "3.83"
             },
             {
                 "name": "Thesis Topic",
@@ -123,11 +152,32 @@ const universities = [
 
 const projects = [
     {
+        "name": "RAPIDO",
+        "type": "C++ library",
+        "image": rapido,
+        "desc": "C++ framework for performing LHC data analysis with easy-to-use tools designed to build a clean and repeatable framework.",
+        "href": "https://github.com/jkguiang/rapido"
+    },
+    {
+        "name": "NBC 7 Investigates",
+        "type": "News Article",
+        "image": nbc,
+        "desc": "Analyzed police employment data for the entire state of California for an NBC 7 Investigates article reporting on the outflux of San Diego police officers.",
+        "href": "https://www.nbcsandiego.com/news/investigations/nbc-7-investigates-san-diego-police-officers-fleeing-the-force/2918582/"
+    },
+    {
+        "name": "Radiology ML",
+        "type": "Preliminary Work",
+        "image": ct_scans,
+        "desc": "Completed preliminary work towards developing convolutional deep-learning algorithms for analyzing CT scans of Covid-19 pneumonia and lung cancer towards clinical utility in collaboration with UCSD radiologists",
+        "href": "https://github.com/sam-may/zephyr"
+    },
+    {
         "name": "Integratable",
         "type": "Web Application",
-        "image": integrals,
+        "image": integratable,
         "desc": "A public tool that provides useful integrals on an interactive, modern platform. Uses a React-based frontend and evaluates known definite integrals using Javascript mathematics functions.",
-        "href": "https://github.com/jkguiang/integratable"
+        "href": "https://jkguiang.github.io/integratable"
     },
     {
         "name": "ChompChapp",
